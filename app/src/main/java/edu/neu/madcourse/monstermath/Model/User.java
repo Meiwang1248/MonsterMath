bfdcee4369e71b4dde689833f38726b92396aee4
Meiwang1248 <meiwang1248@hotmail.com>
2021-07-13 23:27:36 -0700

    Finished initialization

---
+++ app/src/main/java/edu/neu/madcourse/monstermath/Model/User.java
@@ -0,0 +1,37 @@
+package edu.neu.madcourse.monstermath.Model;
+
+import java.util.HashMap;
+
+public class User {
+    private String id;
+    private String username;
+    private String token;
+    public int numOfStickersSent;
+
+    public User(String id, String username, String token) {
+        this.id = id;
+        this.username = username;
+        this.token = token;
+        numOfStickersSent = 0;
+    }
+
+    public User() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getNumOfStickersSent() {
+        return numOfStickersSent;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
