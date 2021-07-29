4006076a3d509eca5606ff8b884b846104b55ab9
Meiwang1248 <meiwang1248@hotmail.com>
2021-07-29 12:53:44 -0700

    Updated GameActivity with validateAnswer() and nextStage()

--- app/src/main/java/edu/neu/madcourse/monstermath/GameActivity.java
+++ app/src/main/java/edu/neu/madcourse/monstermath/GameActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
@@ -13,6 +14,14 @@ public class GameActivity extends AppCompatActivity {
     private Button option3;
     private Button option4;
     private Button option5;
+
+    // images
+    private ImageView m1;
+    private ImageView m2;
+    private ImageView m3;
+    private ImageView m4;
+    private ImageView m5;
+
 
     private Button homeButton;
 
@@ -36,18 +45,24 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
 
-        //connect button and layout
-        option1 = findViewById(R.id.button);
-        option2 = findViewById(R.id.button2);
-        option3 = findViewById(R.id.button3);
-        option4 = findViewById(R.id.button4);
-        option5 = findViewById(R.id.button5);
+        //connect buttons and monsters with layout
+        // installing listener: we can pss the both the answer and monster to
+        option1 = findViewById(R.id.btnAnswer1);
+        m1 = findViewById(R.id.ivMonster1);
+
+        option2 = findViewById(R.id.btnAnswer2);
+        m2 = findViewById(R.id.ivMonster2);
+        option3 = findViewById(R.id.btnAnswer3);
+        m3 = findViewById(R.id.ivMonster3);
+        option4 = findViewById(R.id.btnAnswer4);
+        m4 = findViewById(R.id.ivMonster4);
+        option5 = findViewById(R.id.btnAnswer5);
+        m5 = findViewById(R.id.ivMonster5);
         homeButton = findViewById(R.id.btnCompeteHome);
 
         //connect TextViews and layout
         question = findViewById(R.id.tvQuestion);
         score = findViewById(R.id.tvScoreCount);
-        competitorScore = findViewById(R.id.tvCompetitorScoreCount);
         time = findViewById(R.id.tvTimeCount);
 
     }
@@ -60,9 +75,28 @@ public class GameActivity extends AppCompatActivity {
             // update UI 10 times
             score.setText(game.score);
             question.setText(game.curNumber1+" "+game.operation+" "+game.curNumber2+" = ?");
-
-
         }
         // 1. need to create a pop up window for game over 2. row 58 still need modify 3. each button need a listener
+    }
+
+    /*
+    To be invoked when user click on an answer
+     */
+    private void validateAnswer(int answer) {
+        if (answer == game.curAnswer) {
+            game.score += 10;
+            game.score += game.bonus;
+            if (game.curStage < 10) {
+                nextStage();
+            }
+
+        } else {
+            game.options.remove(answer);
+            // 怪兽要disappear, 怎么知道要消失哪只呢？估计要把答案和怪兽connect起来
+        }
+    }
+
+    private void nextStage() {
+
     }
 }
