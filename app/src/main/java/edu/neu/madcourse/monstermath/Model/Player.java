945e0bc2754bd876643b528cb67e706fb92bb1bc
Zixuan Xu <zxuanxu@hotmail.com>
2021-08-05 11:05:05 +0800

    add matchingmake part

---
+++ app/src/main/java/edu/neu/madcourse/monstermath/Model/Player.java
@@ -0,0 +1,19 @@
+package edu.neu.madcourse.monstermath.Model;
+
+public class Player {
+    private String username;
+    private int score;
+
+    public Player(String username, int score) {
+        this.username = username;
+        this.score = score;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
