c716fc242e104d1cf28671dab9b96d1384418452
Meiwang1248 <meiwang1248@hotmail.com>
2021-08-06 08:23:32 -0700

    starting activity fix

--- app/src/main/java/edu/neu/madcourse/monstermath/StartActivity.java
+++ app/src/main/java/edu/neu/madcourse/monstermath/StartActivity.java
@@ -22,7 +22,7 @@ public class StartActivity extends AppCompatActivity {
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         if (firebaseUser != null) {
-            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            Intent intent = new Intent(StartActivity.this, GameSettingActivity.class);
             startActivity(intent);
             finish();
         }
