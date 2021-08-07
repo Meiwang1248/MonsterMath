655f33c987d73545a2ceadc2b949a10efa4425f1
Zixuan Xu <zxuanxu@hotmail.com>
2021-08-07 11:59:32 +0800

    hide system ui in start activity

--- app/src/main/java/edu/neu/madcourse/monstermath/StartActivity.java
+++ app/src/main/java/edu/neu/madcourse/monstermath/StartActivity.java
@@ -33,6 +33,8 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        hideSystemUI();
+
         btnLogin = findViewById(R.id.login);
         btnRegister = findViewById(R.id.register);
 
@@ -49,5 +51,19 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(new Intent(StartActivity.this, RegisterActivity.class));
             }
         });
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
