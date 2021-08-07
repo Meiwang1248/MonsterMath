bb2f18db60248875b2888ccdebe643ba11a7d98a
RachelLiu6 <42334247+RachelLiu6@users.noreply.github.com>
2021-08-07 16:31:29 -0700

    create dialog class

---
+++ app/src/main/java/edu/neu/madcourse/monstermath/Dialog.java
@@ -0,0 +1,17 @@
+package edu.neu.madcourse.monstermath;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Dialog extends AppCompatDialogFragment {
+
+
+}
