08e22c4132fec63a8271277ad589f910cd9c3a22
Serena Tang <serena@Serenas-MacBook-Pro.local>
2021-08-06 22:50:27 -0700

    hide system ui in other activity

--- app/src/main/java/edu/neu/madcourse/monstermath/GameActivity.java
+++ app/src/main/java/edu/neu/madcourse/monstermath/GameActivity.java
@@ -59,6 +59,8 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_game);
 
+        hideSystemUI();
+
         // get user name
         getUsername();
         // get game settings
@@ -434,5 +436,19 @@ public class GameActivity extends AppCompatActivity {
                 validateAnswer(option5, m5);
             }
         });
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
--- app/src/main/java/edu/neu/madcourse/monstermath/GameSettingActivity.java
+++ app/src/main/java/edu/neu/madcourse/monstermath/GameSettingActivity.java
@@ -20,6 +20,8 @@ public class GameSettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_setting);
 
+        hideSystemUI();
+
         // set onClickListener for toggle button groups
         tgBtnGrpOperation = findViewById(R.id.toggleBtnGrpOperationSelection);
         tgBtnGrpOperation.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
@@ -130,5 +132,19 @@ public class GameSettingActivity extends AppCompatActivity {
     private void openScoreBoard() {
         Intent intent = new Intent(this, ScoreBoardActivity.class);
         startActivity(intent);
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
