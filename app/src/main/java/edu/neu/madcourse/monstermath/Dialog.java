86d8ee7311830d17c461db0560d67331190af598
RachelLiu6 <42334247+RachelLiu6@users.noreply.github.com>
2021-08-07 17:01:16 -0700

    create activity_dialog.xml

--- app/src/main/java/edu/neu/madcourse/monstermath/Dialog.java
+++ app/src/main/java/edu/neu/madcourse/monstermath/Dialog.java
@@ -8,10 +8,27 @@ import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+
 public class Dialog extends AppCompatDialogFragment {
+
+    private int gameScore;
+
+    //constructor
+    public Dialog(int gameScore){
+        this.gameScore = gameScore;
+    }
+
+    @Override
+    public AlertDialog onCreateDialog(Bundle savedInstanceState){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_dialog, null);
+        return builder.create();
+    }
+
 
 
 }
